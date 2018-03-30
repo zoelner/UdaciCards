43c8e9e8e7e13338ac7d87dbeadf7510fc31bb8b
Everton Gabriel Zoelner <everton_zoelner@outlook.com>
2018-03-30 19:09:49 -0300

    ADD Funcao para adicionar baralhos

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
 import { Text, TextInput, View, Button } from "react-native";
 import { StyleSheet } from "react-native";
-import { saveNewDeck } from "./../api";
-
-// TODO
-
-// Uma opção de inserir o título do novo baralho
-// Uma opção de enviar o novo título do baralho e assim criar o baralho
+import { saveNewDeck } from "../utils/api";
+import { NavigationActions } from "react-navigation";
 
 class AddDeck extends Component {
   state = {
     text: ""
   };
+
+  async save(text) {
+    await saveNewDeck(text);
+    this.props.navigation.dispatch(
+      NavigationActions.reset({
+        index: 1,
+        actions: [
+          NavigationActions.navigate({ routeName: "Main" }),
+          NavigationActions.navigate({
+            routeName: "DeckDetail",
+            params: { id: text }
+          })
+        ]
+      })
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -19,7 +32,7 @@ class AddDeck extends Component {
         <TextInput onChangeText={text => this.setState({ text })} />
         <Button
           title="Create Deck"
-          onPress={() => saveNewDeck(this.state.text)}
+          onPress={() => this.save(this.state.text)}
         />
       </View>
     );
