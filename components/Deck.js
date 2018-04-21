bd62fbee0abd1b217b34ea575edd029e7f91b9aa
Everton Gabriel Zoelner <everton_zoelner@outlook.com>
2018-04-21 17:12:42 -0300

    CHANGE Functional Stateless

--- components/Deck.js
+++ components/Deck.js
@@ -2,29 +2,27 @@ import React, { Component } from "react";
 import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
 import { COLOR_BLACK, COLOR_WHITE, COLOR_GRAY } from "../utils/helpers";
 
-class Deck extends Component {
-  render() {
-    return (
-      <View style={styles.deck}>
-        <TouchableOpacity
-          onPress={() =>
-            this.props.navigate("DeckDetail", {
-              id: this.props.title,
-              questions: this.props.questions.length
-            })
-          }
-        >
-          <View style={styles.deckContent}>
-            <Text style={styles.title}>{this.props.title}</Text>
-            <Text>
-              {this.props.questions.length}
-              {this.props.questions.length === 1 ? " question" : " questions"}
-            </Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+function Deck(props) {
+  return (
+    <View style={styles.deck}>
+      <TouchableOpacity
+        onPress={() =>
+          props.navigate("DeckDetail", {
+            id: props.title,
+            questions: props.questions.length
+          })
+        }
+      >
+        <View style={styles.deckContent}>
+          <Text style={styles.title}>{props.title}</Text>
+          <Text>
+            {props.questions.length}
+            {props.questions.length === 1 ? " question" : " questions"}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
